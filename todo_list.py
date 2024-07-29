98dc8b15e6f89dacca1658897d79007935663597
Lazxrus <wedsanter@gmail.com>
2024-07-29 12:03:34 -0400

    Create todo_list.py

---
+++ todo_list.py
@@ -0,0 +1,26 @@
+# Project: Simple To-Do List Application
+
+# Outline:
+# 1. Create a menu-driven console application
+# 2. Implement the following features:
+#    a. Add a task
+#    b. View all tasks
+#    c. Mark a task as complete
+#    d. Remove a task
+#    e. Quit the application
+# 3. Use a list to store tasks
+# 4. Each task should be a dictionary containing:
+#    - Task description
+#    - Completion status (True/False)
+# 5. Implement input validation where necessary
+# 6. Use functions to organize your code
+
+# This project will help you practice:
+# - Working with lists and dictionaries
+# - Creating and calling functions
+# - Implementing a simple menu system
+# - Basic input/output operations
+# - Control structures (if/else, loops)
+
+# To get started, try implementing the menu system and the "Add a task" feature.
+
